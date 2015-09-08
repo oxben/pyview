05d9472bce97fe567ab3ead08831d00226960234
Benjamin Thery <oxben@free.fr>
2015-09-08 14:21:11 +0200

    Lots of changes: multi-frames, events handled in qgraphicsitems, ...

--- pyview.py
+++ pyview.py
@@ -11,16 +11,20 @@ from PyQt4.QtOpenGL import *
 RotOffset   = 5.0
 ScaleOffset = 0.1
 MaxZoom     = 2.0
-FrameRadius = 25.0
+FrameRadius = 15.0
 FrameWidth  = 10.0
+CollageSize = QRectF(0, 0, 1024, 1024)
 
 Debug = True
+OpenGLRender = False
 
 class PhotoFrameItem(QGraphicsItem):
 
     def __init__(self, rect, parent = None, scene = None):
         super(PhotoFrameItem, self).__init__(parent, scene)
         self.rect = rect
+        # Set flags
+        self.setFlags(self.flags() | QGraphicsItem.ItemClipsChildrenToShape)
 
     def boundingRect(self):
         return QRectF(self.rect)
@@ -48,67 +52,54 @@ class PhotoFrameItem(QGraphicsItem):
                                 self.rect.width(), self.rect.height(),
                                 FrameRadius, FrameRadius)
 
-class ImageView(QGraphicsView):
+class PhotoItem(QGraphicsPixmapItem):
 
-    def __init__(self, parent=None):
-        super(ImageView, self).__init__(parent)
-        # Hide scrollbars
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        # Connect signals
-        # Is it necessary?
-        QMetaObject.connectSlotsByName(self)
-        self.dragImage = False
-
-    def setPixmap(self, pixmap):
-        self.pixmap = pixmap
-        origx = self.pixmap.pixmap().size().width()/2
-        origy = self.pixmap.pixmap().size().height()/2
-        print(origx, origy)
-        self.pixmap.setTransformOriginPoint(origx, origy)
+    def __init__(self, pixmap, parent = None, scene = None):
+        super(PhotoItem, self).__init__(pixmap, parent, scene)
+        # Set transform origin to center of pixmap
+        origx = self.pixmap().size().width()/2
+        origy = self.pixmap().size().height()/2
+        self.setTransformOriginPoint(origx, origy)
+        print("origin:", origx, origy)
+        # Use bilinear filtering
+        self.setTransformationMode(Qt.SmoothTransformation)
         # XXX: setTransformationMode() seems buggy for scaling when using setTransform()
         # So translate before and after scaling
         # Example:
         # transform = self.pixmap.transform()
         # transform.translate(256.0, 256.0).scale(1.1, 1.1).rotate(10).translate(-256.0, -256.0)
         # self.pixmap.setTransform(transform)
+        # Set flags
+        self.setFlags(self.flags() |  
+                      QGraphicsItem.ItemIsMovable | 
+                      QGraphicsItem.ItemIsFocusable |
+                      QGraphicsItem.ItemStacksBehindParent)
+        self.setAcceptHoverEvents(True)
 
-        # Use bilinear filtering
-        self.pixmap.setTransformationMode(Qt.SmoothTransformation)
-        print(self.pixmap.transformOriginPoint())
+    def hoverEnterEvent(self, event):
+        # Request keyboard events
+        self.setFocus()
 
-    def setFrame(self, frame):
-        self.frame = frame
+    def hoverLeaveEvent(self, event):
+        self.clearFocus()
 
     def keyReleaseEvent(self, event):
-        global FrameRadius
         if Debug:
             print(event.key())
         if event.key() == Qt.Key_Slash:
             # Reset scale and rotation
-            self.pixmap.setScale(1.0)
-            self.pixmap.setRotation(0.0)
-        elif event.key() == Qt.Key_Plus:
-            FrameRadius += 1.0
-            self.viewport().update()
-        elif event.key() == Qt.Key_Minus:
-            FrameRadius = max(0, FrameRadius - 1.0)
-            self.viewport().update()
+            self.setScale(1.0)
+            self.setRotation(0.0)
 
     def wheelEvent(self, event):
-        scale = self.pixmap.scale()
-        rot   = self.pixmap.rotation()
+        scale = self.scale()
+        rot   = self.rotation()
         if event.delta() > 0:
             if Debug:
                 print("Zoom")
             rot += RotOffset
             if scale < MaxZoom:
                 scale += ScaleOffset
-
-            if event.modifiers() == Qt.ShiftModifier:
-                self.pixmap.setRotation(self.pixmap.rotation() + RotOffset)
-            elif self.pixmap.scale() < 2.0:
-                self.pixmap.setScale(self.pixmap.scale() + ScaleOffset)
         else:
             if Debug:
                 print("Unzoom")
@@ -118,25 +109,56 @@ class ImageView(QGraphicsView):
 
         modifiers = event.modifiers()
         if modifiers == Qt.NoModifier:
-            self.pixmap.setScale(scale)
+            self.setScale(scale)
         elif modifiers == Qt.ShiftModifier:
-            self.pixmap.setRotation(rot)
+            self.setRotation(rot)
         elif modifiers == (Qt.ShiftModifier|Qt.ControlModifier):
-            self.pixmap.setScale(scale)
-            self.pixmap.setRotation(rot)
+            self.setScale(scale)
+            self.setRotation(rot)
 
-    def mouseMoveEvent(self, event):
-        if event.buttons() == Qt.LeftButton:
-            self.pixmap.setPos(self.pixmap.pos() + (event.posF() - self.dragOrig))
-            self.dragOrig = event.posF()
+    # def mouseMoveEvent(self, event):
+    #     if event.buttons() == Qt.LeftButton:
+    #         self.setPos(self.pos() + (event.pos() - self.dragOrig))
+    #         self.dragOrig = event.pos()
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            if Debug:
-                print("Left button pressed")
-            self.dragOrig = event.posF()
+    # def mousePressEvent(self, event):
+    #     if event.button() == Qt.LeftButton:
+    #         if Debug:
+    #             print("Left button pressed")
+    #         self.dragOrig = event.pos()
+    #         app.setOverrideCursor(Qt.ClosedHandCursor)
 
-    # def mouseReleaseEvent (self, QMouseEvent event):
+    # def mouseReleaseEvent (self, event):
+    #     if event.button() == Qt.LeftButton:
+    #         app.restoreOverrideCursor()
+
+
+class ImageView(QGraphicsView):
+
+    def __init__(self, parent=None):
+        super(ImageView, self).__init__(parent)
+        # Hide scrollbars
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        # Connect signals: XXX: Is it necessary?
+        #QMetaObject.connectSlotsByName(self)
+
+    def keyReleaseEvent(self, event):
+        global FrameRadius
+        if Debug:
+            print(event.key())
+        if event.key() == Qt.Key_Plus:
+            FrameRadius += 1.0
+            self.viewport().update()
+        elif event.key() == Qt.Key_Minus:
+            FrameRadius = max(0, FrameRadius - 1.0)
+            self.viewport().update()
+        else:
+        # Pass event to default handler
+            super(ImageView, self).keyReleaseEvent(event)
+    
+    def resizeEvent(self, event):
+        self.fitInView(CollageSize, Qt.KeepAspectRatio)
 
 #
 # Main
@@ -151,20 +173,41 @@ w = QWidget()
 # Set window title
 w.setWindowTitle("PyView")
 w.resize(512, 512)
+layout = QVBoxLayout()
+w.setLayout(layout)
 
 # Create GraphicsView
-gfxview = ImageView(w)
+gfxview = ImageView()
+layout.addWidget(gfxview)
+gfxview.setBackgroundBrush(QBrush(Qt.white))
+
 # Set OpenGL renderer
-#gfxview.setViewport(QGLWidget())
+if OpenGLRender:
+    gfxview.setViewport(QGLWidget())
+
 # Add scene
 scene = QGraphicsScene()
+
 # Load pixmap and add it to the scene
-pixmap = QPixmap(os.getcwd() + '/test.png')
-pix = scene.addPixmap(pixmap)
-gfxview.setPixmap(pix)
-frame = PhotoFrameItem(QRect(0, 0, 512, 512))
-fr = scene.addItem(frame)
-gfxview.setFrame(fr)
+photosX = 2
+photosY = 3
+photoWidth  = CollageSize.width() / photosX
+photoHeight =  CollageSize.height() / photosY
+print("photoWidth=", photoWidth)
+print("photoHeight=", photoHeight)
+for x in range (0, photosX):
+    for y in range (0, photosY):
+        # Create frame and photo
+        frame = PhotoFrameItem(QRect(0, 0, photoWidth, photoHeight))
+        frame.setPos(x * photoWidth, y * photoHeight)
+        photo = PhotoItem(QPixmap(os.getcwd() + '/test.png'))
+        photo.setParentItem(frame)
+        # Center photo in frame
+        photo.setPos(photoWidth/2 - photo.pixmap().width()/2,
+                     photoHeight/2 - photo.pixmap().height()/2)
+        # Add frame to scene
+        fr = scene.addItem(frame)
+
 gfxview.setScene(scene)
 
 # Show window
